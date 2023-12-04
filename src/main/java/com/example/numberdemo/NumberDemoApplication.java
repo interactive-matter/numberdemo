95a6047ba0972f40d915cc65683cb07e947c3236
Marcus Erhardt <marcus@nicht-im-internet.de>
2023-12-04 09:02:49 +0100

    lets start with spring boot starter temaplte

---
+++ src/main/java/com/example/numberdemo/NumberDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.numberdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberDemoApplication.class, args);
+	}
+
+}
