0c4ccc91aca75df61530ec429eadc1d48be8d93f
Marcus Erhardt <marcus@nicht-im-internet.de>
2023-12-04 14:48:41 +0100

    the implementation of the convert was rather straight forward

--- src/main/java/com/example/numberdemo/Converter.java
+++ src/main/java/com/example/numberdemo/Converter.java
@@ -2,15 +2,41 @@ package com.example.numberdemo;
 
 import com.example.numberdemo.inputters.Inputter;
 import com.example.numberdemo.outputters.Outputter;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+
+@Slf4j
 public class Converter {
-    void registerInputter(String name, Inputter inputter) {
+    private final Map<String, Inputter> inputters = new HashMap<>();
+    private final Map<String, Outputter> outputters = new HashMap<>();
+
+    public final void registerInputter(String name, Inputter inputter) {
+        Inputter current = inputters.putIfAbsent(name, inputter);
+        if (current != null) {
+            throw new IllegalArgumentException("There is already an inputter named " + name + ":" + current);
+        }
     }
 
-    void registerOutputter(String name, Outputter outputter) {
+    public void registerOutputter(String name, Outputter outputter) {
+        Outputter current = outputters.putIfAbsent(name, outputter);
+        if (current != null) {
+            throw new IllegalArgumentException("There is already an inputter named " + name + ":" + current);
+        }
     }
 
-    void convert(String inputter, String outputter, String number) {
-
+    public String convert(String inputter, String outputter, String number) {
+        Integer intRep = ofNullable(inputters.get(inputter)).map(i -> i.apply(number)).orElseThrow(
+                () -> new IllegalArgumentException("There is no inputter by the name `" + inputter + "`")
+        );
+        String result = ofNullable(outputters.get(outputter)).map(o -> o.apply(intRep)).orElseThrow(
+                () -> new IllegalArgumentException("There is no outputter by the name `" + inputter + "`")
+        );
+        log.info("Read {} with {} to {}. Converting it to {} with {}", number, inputter, intRep, result, outputter);
+        return result;
     }
 }
--- src/test/groovy/com/example/numberdemo/ConverterSpec.groovy
+++ src/test/groovy/com/example/numberdemo/ConverterSpec.groovy
@@ -4,7 +4,7 @@ import com.example.numberdemo.inputters.Inputter
 import spock.lang.Specification
 
 class ConverterSpec extends Specification{
-    def "test if registering inuts and outputs worka"() {
+    def "test if registering inputs and outputs works"() {
         def converter = new Converter()
 
         when:
@@ -12,7 +12,63 @@ class ConverterSpec extends Specification{
         converter.registerOutputter "y", {number -> Integer.toString(number)}
 
         then:
-        converter.convert ("x","y","test") == test
+        converter.convert ("x","y","test") == "-1"
+
+    }
+
+    def "test if registering inputs twice leads to an exception"() {
+        def converter = new Converter()
+
+        when:
+        converter.registerInputter "x", { number -> -1}
+        converter.registerOutputter "y", {number -> Integer.toString(number)}
+        converter.registerInputter "x", { number -> -9}
+
+
+        then:
+        thrown IllegalArgumentException
+
+        and:
+        converter.convert ("x","y","test") == "-1"
+
+
+    }
+
+
+    def "test if registering outputters twice leads to an exception"() {
+        def converter = new Converter()
+
+        when:
+        converter.registerInputter "x", { number -> -1}
+        converter.registerOutputter "y", {number -> Integer.toString(number)}
+        converter.registerOutputter "y", {number -> "wrong"}
+
+        then:
+        thrown IllegalArgumentException
+
+        and:
+        converter.convert ("x","y","test") == "-1"
+
+    }
+
+    def "test if accessing wrong inputs and outputs throws exception"() {
+        def converter = new Converter()
+
+        when:
+        converter.registerInputter "x", { number -> -1}
+        converter.registerOutputter "y", {number -> Integer.toString(number)}
+
+        and:
+        converter.convert ("a","y","test")
+
+        then:
+        thrown IllegalArgumentException
+
+        when:
+        converter.convert ("x","a","test")
+
+        then:
+        thrown IllegalArgumentException
 
     }
 }
