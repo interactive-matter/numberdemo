7d0f5198824ae4fb20bf6de4a53b03fe21899d1b
Marcus Erhardt <marcus@nicht-im-internet.de>
2023-12-04 20:51:02 +0100

    optimising the map initialization a bit - I hate static code

--- src/main/java/com/example/numberdemo/outputters/RomanOutputter.java
+++ src/main/java/com/example/numberdemo/outputters/RomanOutputter.java
@@ -1,26 +1,27 @@
 package com.example.numberdemo.outputters;
 
+import java.util.Map;
+import java.util.SortedMap;
 import java.util.TreeMap;
 
-//this was quite a nice implementation chat GPT cam up with - like it
+//this was quite a nice implementation chat GPT came up with - like it
 public class RomanOutputter implements Outputter{
-    private static final TreeMap<Integer, String> ROMAN_MAP = new TreeMap<>((a, b) -> b - a);
-
-    static {
-        ROMAN_MAP.put(1000, "M");
-        ROMAN_MAP.put(900, "CM");
-        ROMAN_MAP.put(500, "D");
-        ROMAN_MAP.put(400, "CD");
-        ROMAN_MAP.put(100, "C");
-        ROMAN_MAP.put(90, "XC");
-        ROMAN_MAP.put(50, "L");
-        ROMAN_MAP.put(40, "XL");
-        ROMAN_MAP.put(10, "X");
-        ROMAN_MAP.put(9, "IX");
-        ROMAN_MAP.put(5, "V");
-        ROMAN_MAP.put(4, "IV");
-        ROMAN_MAP.put(1, "I");
-    }
+    private static final TreeMap<Integer, String> ROMAN_MAP = new TreeMap<>(
+            (a, b) -> b - a) {{
+        put(1000, "M");
+        put(900, "CM");
+        put(500, "D");
+        put(400, "CD");
+        put(100, "C");
+        put(90, "XC");
+        put(50, "L");
+        put(40, "XL");
+        put(10, "X");
+        put(9, "IX");
+        put(5, "V");
+        put(4, "IV");
+        put(1, "I");
+    }};
     @Override
     public String apply(Integer number) {
 
